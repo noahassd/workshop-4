c53c4f773ab45de1a007114ed6f10484815cabc4
noahASD <37837626+Nh161103@users.noreply.github.com>
2025-03-17 18:47:53 +0100

    part 1

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -9,7 +9,9 @@ export async function simpleOnionRouter(nodeId: number) {
 
   // TODO implement the status route
   // onionRouter.get("/status", (req, res) => {});
-
+  onionRouter.get("/status", (req, res) => {
+    res.status(200).send("live");
+  });
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
       `Onion router ${nodeId} is listening on port ${
